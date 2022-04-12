6e99d87a8a141c82b8a7200a276d36762b4742b3
andavi <andrew.bradley.davis@gmail.com>
2022-04-12 00:09:56 -0700

    refactor sans babel

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
+  state = {
+    products: [],
+  };
+  
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
   
-  handleProductUpVote(productId) {
-    // console.log(productId + ' was upvoted.');
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -54,15 +48,18 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
+  //   this.handleUpVote = this.handleUpVote.bind(this);
+  // }
 
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  // handleUpVote() {
+  //   this.props.onVote(this.props.id);
+  // }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
